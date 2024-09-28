749b121ce7fed04206413abc4028765f35ff63b6
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-28 10:58:43 +0800

    responsive grid

--- src/app/curl-builder/curl-builder.component.ts
+++ src/app/curl-builder/curl-builder.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -6,6 +7,12 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
+interface ColSpans {
+  method: number;
+  url: number;
+  body: number;
+}
+
 @Component({
   selector: 'curl-builder',
   standalone: true,
@@ -13,6 +20,7 @@ import { MatSelectModule } from '@angular/material/select';
   templateUrl: './curl-builder.component.html',
   styleUrl: './curl-builder.component.scss'
 })
+
 export class CurlBuilderComponent {
   methods: string[] = [
     "GET",
@@ -21,4 +29,24 @@ export class CurlBuilderComponent {
     "PATCH",
     "DELETE",
   ]
+  isMobile: boolean = false;
+  colspans: ColSpans = {
+    method: 3,
+    url: 9,
+    body: 12
+  }
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Handset
+    ]).subscribe(result => {
+      this.isMobile = result.matches;
+      this.colspans = {
+        method: this.isMobile ? 12 : 3,
+        url: this.isMobile ? 12 : 9,
+        body: 12
+      }
+    });
+  }
+
 }
