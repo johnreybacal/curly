69badcb3f253c3d3a444aa21437ff5f3840130e2
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-28 10:04:04 +0800

    basic curl form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { CurlBuilderComponent } from './curl-builder/curl-builder.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CurlBuilderComponent],
+  imports: [RouterOutlet, CurlBuilderComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/curl-builder/curl-builder.component.ts
+++ src/app/curl-builder/curl-builder.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'curl-builder',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatGridListModule],
   templateUrl: './curl-builder.component.html',
   styleUrl: './curl-builder.component.css'
 })
 export class CurlBuilderComponent {
-
+  methods: string[] = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+  ]
 }
