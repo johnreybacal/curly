fdcce8efeac828b54abc47ee88e44c36f708dd42
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-28 10:36:07 +0800

    improve curl form

--- src/app/curl-builder/curl-builder.component.ts
+++ src/app/curl-builder/curl-builder.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
@@ -8,7 +9,7 @@ import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'curl-builder',
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatGridListModule],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatGridListModule, MatCardModule],
   templateUrl: './curl-builder.component.html',
   styleUrl: './curl-builder.component.scss'
 })
