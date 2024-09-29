e2a3bf23f1fb4eb89e522d185c33ceab7b366643
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-29 10:30:23 +0800

    group actions

--- src/app/property-builder/property-builder.component.ts
+++ src/app/property-builder/property-builder.component.ts
@@ -1,9 +1,12 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatTable, MatTableModule } from '@angular/material/table';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 export interface Data {
   field: string;
@@ -18,27 +21,46 @@ const ELEMENT_DATA: Data[] = [
 @Component({
   selector: 'property-builder',
   standalone: true,
-  imports: [MatTableModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatIcon
+  ],
   templateUrl: './property-builder.component.html',
   styleUrl: './property-builder.component.scss'
 })
 export class PropertyBuilderComponent {
-  displayedColumns: string[] = ['field', 'value'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['field', 'value', 'select', 'remove'];
+  dataSource = new MatTableDataSource<Data>(ELEMENT_DATA);
+  selection = new SelectionModel<Data>(true, []);
 
 
   @ViewChild(MatTable) table?: MatTable<Data>;
 
   addData() {
-    this.dataSource.push({
+    this.dataSource.data.push({
       field: "",
       value: ""
     });
     this.table?.renderRows();
   }
 
-  getData() {
-    console.log(this.dataSource)
+  removeData(index: number) {
+    this.dataSource.data.splice(index, 1)
+    this.table?.renderRows();
   }
+
+  getData() {
+    console.log(this.selection.selected)
+  }
+
+  checkboxLabel(row: Data): string {
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'}`;
+  }
+
 
 }
